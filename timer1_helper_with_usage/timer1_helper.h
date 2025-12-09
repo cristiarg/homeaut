0f581ddee20594db86da600d7d0bc2065819ef36
Cristian Arghiroiu <cristi.arghiroiu@gmail.com>
2025-12-10 00:21:40 +0200

    a TIMER1 helper class together with a scketch showing various practical usage examples

---
+++ timer1_helper_with_usage/timer1_helper.h
@@ -0,0 +1,388 @@
+#pragma once
+#include <Arduino.h>
+
+/**
+ * "Static" class. Provides a, somewhat useful, interface to the TIMER1 capabilities.
+ *
+ * Allows to use:
+ *  - usage of both comparers A and B, each one separately or simultaneously
+ *  - explicit error return codes for some frequent mis-handlings
+ *      - especially useful is the usage of incompatible prescale value for the A, B
+ *        comparers which both share the same (single) prescaler
+ *  - fire-once, fire-multiple and indefinite-firing callback handling
+ *
+ * Notes about the prescaler:
+ *  - On a standard 16 MHz Arduino (Uno/Nano/etc.), Timer1 is 16-bit, so the longest delay
+ *    you can get in normal mode is when the counter runs from 0 to 65535 and overflows. 
+ *  - This formula gives the maximum possible interval of a prescaler divisor mode:
+ *      ```
+ *      Max interval (ms) = ((prescaler * 65536) / 16000000) * 1000
+ *      ```
+ *  - Maximum delays possible with each prescaler before the timer overflows:
+ *      ------------------------------
+ *      Prescaler  | Max delay (ms)
+ *      -----------+------------------
+ *             1   |      4.096
+ *             8   |     32.768
+ *            64   |    262.144
+ *           256   |   1048.576
+ *          1024   |   4194.304
+ *      ------------------------------
+ *
+ * Nice to have (but which we don't have yet):
+ *  - possibility to hand over user data with the callback
+ *  - accept a pattern of delays in the form of an array and have the routine fired at
+ *    those specified delays (maybe even in a round-robin fashion)
+ *  - support also the Overflow Interrupot of TIMER1 (TOIE1)
+ *      - https://wolles-elektronikkiste.de/en/interrupts-part-3-timer-interrupts -
+ *        see "Using multiple Timer1 interruputs" where all 3 can be used
+ *
+ * Resources:
+ *  - https://wolles-elektronikkiste.de/en/timer-and-pwm-part-2-16-bit-timer1
+ *  - https://wolles-elektronikkiste.de/en/interrupts-part-3-timer-interrupts
+ */
+class Timer1Helper {
+public:
+    typedef void (*Callback)();
+
+    /**
+     * no error
+     */
+    static const int8_t ER_OK = 0;
+    /**
+     * The delay is too large for any prescaler of TIMER1
+     */
+    static const int8_t ER_PRESCALER_OUT_OF_BOUND = 1;
+    /**
+     * An already existing prescaler value is set and a new calculated one is
+     * incompatible (both A and B comparers share the single prescaler of TIMER1).
+     */
+    static const int8_t ER_INCOMPATIBLE_A_B_PRESCALE = 2;
+    /**
+     * Recurrence value must be a strictly positive integer.
+     */
+    static const int8_t ER_INVALID_RECURRENCE_VALUE = 3;
+    /**
+     * A callback cannot be null
+     */
+    static const int8_t ER_NULL_CALLBACK = 4;
+
+public:
+    Timer1Helper()
+    {
+    }
+
+    /**
+     * Reset/stop any working interrupts on both comparers
+     */
+    static void reset() {
+        cli(); // CLear Interrupts
+
+        Timer1Helper::prescaler_bits = 0;
+
+        Timer1Helper::cbA = nullptr;
+        Timer1Helper::cbA_recurrence = 0;
+        Timer1Helper::cbA_compare_value = 0;
+
+        Timer1Helper::cbB = nullptr;
+        Timer1Helper::cbB_recurrence = 0;
+        Timer1Helper::cbB_compare_value = 0;
+
+        TCCR1A = 0;
+        TCCR1B = 0; // Stop timer
+        TIMSK1 = 0; // Disable all Timer1 interrupts (A, B comparers and overflow)
+        TCNT1 = 0; // counter
+        TIFR1 = 0; // clear occured interrupt flag register
+
+        sei(); // SEt Interrupts
+    }
+
+    /**
+     * Reset/stop TIMER1 comparer A interrupt; do not touch B
+     */
+    static void resetA() {
+        cli();
+
+        Timer1Helper::cbA = nullptr;
+        Timer1Helper::cbA_recurrence = 0;
+        Timer1Helper::cbA_compare_value = 0;
+
+        TIMSK1 &= ~(1 << OCIE1A);
+        OCR1A = 0;
+        TIFR1 &= ~(1 << OCF1A);
+
+        sei();
+    }
+
+    /**
+     * Reset/stop TIMER1 comparer B interrupt; do not touch A
+     */
+    static void resetB() {
+        cli();
+
+        Timer1Helper::cbB = nullptr;
+        Timer1Helper::cbB_recurrence = 0;
+        Timer1Helper::cbB_compare_value = 0;
+
+        TIMSK1 &= ~(1 << OCIE1B);
+        OCR1B = 0;
+        TIFR1 &= ~(1 << OCF1B);
+
+        sei();
+    }
+
+    /**
+     * Schedule TIMER1 comparer A to fire once at @delay_ms from 'now'.
+     *
+     * @return error code (see class static constants)
+     */
+    static int8_t scheduleAOnce(uint32_t delay_ms, Callback cb) {
+        return doScheduleA(delay_ms, 1, cb);
+    }
+
+    /**
+     * Schedule TIMER1 comparer A to fire @cb callback a @recurrence number of times (equidistant
+     * in time) with first firing at @delay_ms from 'now'.
+     *
+     * @return error code (see class static constants)
+     */
+    static int8_t scheduleARecurrent(uint32_t delay_ms, int8_t recurrence, Callback cb) {
+        if (recurrence <= 0) {
+            return ER_INVALID_RECURRENCE_VALUE;
+        }
+        return doScheduleA(delay_ms, recurrence, cb);
+    }
+
+    /**
+     * Schedule TIMER1 comparer B to fire @cb callback an indefinite number of times (equidistant
+     * in time) with first firing at @delay_ms from 'now'.
+     *
+     * @return error code (see class static constants)
+     */
+    static int8_t scheduleAIndefinitely(uint32_t delay_ms, Callback cb) {
+        return doScheduleA(delay_ms, -1, cb);
+    }
+
+    /**
+     * Schedule TIMER1 comparer B to fire once at @delay_ms from 'now'.
+     *
+     * @return error code (see class static constants)
+     */
+    static int8_t scheduleBOnce(uint32_t delay_ms, Callback cb) {
+        return doScheduleB(delay_ms, 1, cb);
+    }
+
+    /**
+     * Schedule TIMER1 comparer B to fire @cb callback a @recurrence number of times (equidistant
+     * in time) with first firing at @delay_ms from 'now'.
+     *
+     * @return error code (see class static constants)
+     */
+    static int8_t scheduleBRecurrent(uint32_t delay_ms, int8_t recurrence, Callback cb) {
+        if (recurrence <= 0) {
+            return ER_INVALID_RECURRENCE_VALUE;
+        }
+        return doScheduleB(delay_ms, recurrence, cb);
+    }
+
+    /**
+     * Schedule TIMER1 comparer B to fire @cb callback an indefinite number of times (equidistant
+     * in time) with first firing at @delay_ms from 'now'.
+     *
+     * @return error code (see class static constants)
+     */
+    static int8_t scheduleBIndefinitely(uint32_t delay_ms, Callback cb) {
+        return doScheduleB(delay_ms, -1, cb);
+    }
+
+private:
+    static int8_t doScheduleA(uint32_t delay_ms, int8_t recurrence, Callback cb) {
+        if (cb == NULL) {
+            return ER_NULL_CALLBACK;
+        }
+        // TODO:
+        //  - race condition with possible existing setting; how to fix?
+        //  - alleviate by manipulating TIMSK1 and re-setting / setting flag OCIE1A as appropriate
+        const int8_t err_code = setupCompare(delay_ms, A);
+        if (err_code == ER_OK) {
+            Timer1Helper::cbA_recurrence = recurrence;
+            Timer1Helper::cbA = cb;
+        }
+        return err_code;
+    }
+
+    static int8_t doScheduleB(uint32_t delay_ms, int8_t recurrence, Callback cb) {
+        if (cb == NULL) {
+            return ER_NULL_CALLBACK;
+        }
+        // TODO:
+        //  - race condition with possible existing setting; how to fix?
+        //  - alleviate by manipulating TIMSK1 and re-setting / setting flag OCIE1B as appropriate
+        const int8_t err_code = setupCompare(delay_ms, B);
+        if (err_code == ER_OK) {
+            Timer1Helper::cbB_recurrence = recurrence;
+            Timer1Helper::cbB = cb;
+        }
+        return err_code;
+    }
+
+private:
+    // which comparers to activate
+    // TODO: also add overflow TO1E1, apparently all can work together - see
+    // https://wolles-elektronikkiste.de/en/interrupts-part-3-timer-interrupts - Using multiple Timer1 interrupts
+    enum Channel {
+        A = 0,
+        B = 1
+    };
+
+public: // statics
+    /**
+     * the prescaler is shared between the two comparers; this static is part of a
+     * mechanism to ensure that the two delays are not imcompatible when different delay
+     * intervals are requested
+     */
+    static uint16_t prescaler_bits;
+    static Callback cbA;
+    /**
+     * < 0 : infinite recurrence
+     * == 0: invalid value; it is actually the idle value
+     * > 0 : specifies the number of recurrences
+     */
+    static int8_t cbA_recurrence;
+    static uint16_t cbA_compare_value;
+
+    static Callback cbB;
+    static int8_t cbB_recurrence;
+    static uint16_t cbB_compare_value;
+
+private:
+    static int8_t setupCompare(uint32_t delay_ms, Channel ch) {
+        // Convert ms to timer ticks and choose prescaler
+        const uint32_t ticks = (F_CPU / 1000UL) * delay_ms; // raw ticks - the range in ticks that we need to have in the prescaler to be able to accomodate the required delay
+        uint16_t prescaleBits = 0;
+        uint16_t compareValue = 0;
+
+        if (choosePrescaler(ticks, prescaleBits, &compareValue)) {
+            if (Timer1Helper::prescaler_bits != 0 && Timer1Helper::prescaler_bits != prescaleBits) {
+                return ER_INCOMPATIBLE_A_B_PRESCALE;
+            }
+            // TODO: for now, the prescaler is only reset with the reset function; it would be
+            // possible to implement a RAII-type usage of the prescaler (this would be useful with the
+            // fire-once working method) but first need to find out about //#include <util/atomic.h>
+            Timer1Helper::prescaler_bits = prescaleBits;
+
+            const uint16_t now = TCNT1;
+            const uint16_t target = now + compareValue;
+            if (ch == A) {
+                Timer1Helper::cbA_compare_value = compareValue;
+                OCR1A = target;
+                TIMSK1 |= (1 << OCIE1A);
+            } else {
+                Timer1Helper::cbB_compare_value = compareValue;
+                OCR1B = target;
+                TIMSK1 |= (1 << OCIE1B);
+            }
+            // Start timer with shared prescaler
+            TCCR1B = prescaleBits;
+            // NOTE: TCCR1B |= (1 << WGM12) sets CTC mode (Clear Timer on Compare Mode 4)
+            // which results in the timer counter only counting up to OCR1A and then
+            // is deactivated - https://wolles-elektronikkiste.de/wp-content/uploads/2020/01/WGM1.png ;
+            // anyway, the result is that only the A routine would be fired and not both
+
+            return ER_OK;
+        } else {
+            // Too long delay, cap it or ignore
+            return ER_PRESCALER_OUT_OF_BOUND;
+        }
+    }
+
+    static bool choosePrescaler(uint32_t ticks, uint16_t &bits, uint16_t *compare) {
+        static const struct {
+          uint16_t bits;
+          uint16_t div;
+        } prescale_arr[] = {
+          {1, 1},
+          {2, 8},
+          {3, 64},
+          {4, 256},
+          {5, 1024}
+        };
+        for (auto &prescale : prescale_arr) {
+            uint32_t t = ticks / prescale.div;
+            if (t <= 65535) { // TIMER1 is 16 bits
+                bits = prescale.bits;
+                *compare = (uint16_t)t;
+                return true;
+            }
+        }
+        return false;
+    }
+};
+
+// static members
+uint16_t Timer1Helper::prescaler_bits = 0;
+
+Timer1Helper::Callback Timer1Helper::cbA = nullptr;
+int8_t Timer1Helper::cbA_recurrence = 0;
+uint16_t Timer1Helper::cbA_compare_value = 0;
+
+Timer1Helper::Callback Timer1Helper::cbB = nullptr;
+int8_t Timer1Helper::cbB_recurrence = 0;
+uint16_t Timer1Helper::cbB_compare_value = 0;
+
+// ISRs
+ISR(TIMER1_COMPA_vect) {
+    // NOTE: any of the static values corresponding to a comparer can be used
+    // as a flag/marker (callback, recurrence value) and we chose it to be the
+    // recurrence value.
+    if (/*Timer1Helper::cbA != NULL &&*/ Timer1Helper::cbA_recurrence != 0) {
+        auto fn = Timer1Helper::cbA;
+
+        // example with 3 recurrence:
+        // 3->2, cb; 2->1, cb; 1->0, deactivate, cb
+        if (Timer1Helper::cbA_recurrence > 0) {
+            Timer1Helper::cbA_recurrence -= 1;
+        }
+        if (Timer1Helper::cbA_recurrence == 0) {
+            TIMSK1 &= ~(1 << OCIE1A);
+            Timer1Helper::cbA = nullptr;
+        }
+
+        // adjust Output Compare Register A for correct next firing according to calculated delay
+        // (makes heavy use of math overflow of the 16 bit register)
+        const uint16_t now = TCNT1;
+        const uint16_t target = now + Timer1Helper::cbA_compare_value;
+        OCR1A = target;
+
+        fn();
+    } else {
+        // here be dragons; this would be an inconsistent state resulting from potential
+        // race conditions; when new values are set for one of the comparers, this is
+        // done in a hot-swap manner, without actually disabling interrupt handling
+        // for that comparer (if enabled)
+        // TODO: we could temporary disable the corresponding flag in TIMSK1 - to be investigated
+    }
+}
+
+ISR(TIMER1_COMPB_vect) {
+    if (/*Timer1Helper::cbB != NULL &&*/ Timer1Helper::cbB_recurrence != 0) {
+        auto fn = Timer1Helper::cbB;
+
+        if (Timer1Helper::cbB_recurrence > 0) {
+            Timer1Helper::cbB_recurrence -= 1;
+        }
+        if (Timer1Helper::cbB_recurrence == 0) {
+            TIMSK1 &= ~(1 << OCIE1B);
+            Timer1Helper::cbB = nullptr;
+            Timer1Helper::cbB_compare_value = 0;
+        }
+
+        // adjust Output Compare Register B for correct next firing according to calculated delay
+        // (makes heavy use of math overflow of the 16 bit register)
+        const uint16_t now = TCNT1;
+        const uint16_t target = now + Timer1Helper::cbB_compare_value;
+        OCR1B = target;
+
+        fn();
+    }
+}
---
+++ timer1_helper_with_usage/timer1_helper_with_usage.ino
@@ -0,0 +1,334 @@
+
+// #include <avr/sleep.h>
+// #include <avr/power.h>
+
+#include "timer1_helper.h"
+
+static Timer1Helper timer1;
+
+uint32_t delay_ms = 50;
+
+void example_fire_once_channels_a_b() {
+    int8_t err_code = 0;
+
+    timer1.reset(); // clean before
+
+    Serial.print("sta ");
+    Serial.println(millis());
+
+    err_code = timer1.scheduleAOnce(350, []() {
+        // WORK: do something
+        Serial.print("end A ");
+        Serial.println(millis());
+    });
+    if (err_code != Timer1Helper::ER_OK) {
+        Serial.print("ERR: A ");
+        Serial.println(err_code);
+    }
+
+    err_code = timer1.scheduleBOnce(800, []() {
+        // WORK: do something
+        Serial.print("end B ");
+        Serial.println(millis());
+    });
+    if (err_code != Timer1Helper::ER_OK) {
+        Serial.print("ERR: B ");
+        Serial.println(err_code);
+    }
+}
+
+void example_where_prescaler_calculates_incompatible_value_between_the_two_channels_and_will_not_be_set() {
+    int8_t err_code = 0;
+
+    timer1.reset(); // clean before
+
+    Serial.print("sta ");
+    Serial.println(millis());
+
+    err_code = timer1.scheduleAOnce(900, []() {
+        // WORK: do something
+        Serial.print("end A ");
+        Serial.println(millis());
+    });
+    if (err_code != Timer1Helper::ER_OK) {
+        Serial.print("ERR: A ");
+        Serial.println(err_code);
+    }
+
+    err_code = timer1.scheduleBOnce(1200, []() {
+        // WORK: do something
+        Serial.print("end B ");
+        Serial.println(millis());
+    });
+    if (err_code != Timer1Helper::ER_OK) {
+        Serial.print("ERR: B ");
+        Serial.println(err_code);
+    }
+}
+
+void example_where_delay_is_beyond_the_upper_max_for_timer1() {
+    int8_t err_code = 0;
+
+    timer1.reset(); // clean before
+
+    Serial.print("sta ");
+    Serial.println(millis());
+
+    err_code = timer1.scheduleBOnce(4195, []() {
+        // WORK: do something
+        Serial.print("end B ");
+        Serial.println(millis());
+    });
+    if (err_code != Timer1Helper::ER_OK) {
+        Serial.print("ERR: B ");
+        Serial.println(err_code);
+    }
+}
+
+void example_fire_recurrent_only_channel_A() {
+    int8_t err_code = 0;
+
+    timer1.reset(); // clean before
+
+    Serial.print("sta ");
+    Serial.println(millis());
+
+    err_code = timer1.scheduleARecurrent(500, 25, []() {
+        // WORK: do something
+        Serial.print("end A ");
+        Serial.println(millis());
+    });
+    if (err_code != Timer1Helper::ER_OK) {
+        Serial.print("ERR: A ");
+        Serial.println(err_code);
+    }
+}
+
+void example_fire_recurrent_only_channel_B() {
+    int8_t err_code = 0;
+
+    timer1.reset(); // clean before
+
+    Serial.print("sta ");
+    Serial.println(millis());
+
+    err_code = timer1.scheduleBRecurrent(938, 8, []() {
+        // WORK: do something
+        Serial.print("end B ");
+        Serial.println(millis());
+    });
+    if (err_code != Timer1Helper::ER_OK) {
+        Serial.print("ERR: B ");
+        Serial.println(err_code);
+    }
+}
+
+void example_fire_recurrent_channels_A_B() {
+    int8_t err_code = 0;
+
+    timer1.reset(); // clean before
+
+    Serial.print("sta ");
+    Serial.println(millis());
+
+    err_code = timer1.scheduleARecurrent(500, 15, []() {
+        // WORK: do something
+        Serial.print("end A ");
+        Serial.println(millis());
+    });
+    if (err_code != Timer1Helper::ER_OK) {
+        Serial.print("ERR: A ");
+        Serial.println(err_code);
+    }
+
+    err_code = timer1.scheduleBRecurrent(938, 8, []() {
+        // WORK: do something
+        Serial.print("end B ");
+        Serial.println(millis());
+    });
+    if (err_code != Timer1Helper::ER_OK) {
+        Serial.print("ERR: B ");
+        Serial.println(err_code);
+    }
+}
+
+void example_fire_recurrent_channels_A_B_indenfinitely() {
+    int8_t err_code = 0;
+
+    timer1.reset(); // clean before
+
+    Serial.print("sta ");
+    Serial.println(millis());
+
+    err_code = timer1.scheduleAIndefinitely(19, []() {
+        // WORK: do something
+        Serial.print("end A ");
+        Serial.println(millis());
+    });
+    if (err_code != Timer1Helper::ER_OK) {
+        Serial.print("ERR: A ");
+        Serial.println(err_code);
+    }
+
+    err_code = timer1.scheduleBIndefinitely(31, []() {
+        // WORK: do something
+        Serial.print("end B ");
+        Serial.println(millis());
+    });
+    if (err_code != Timer1Helper::ER_OK) {
+        Serial.print("ERR: B ");
+        Serial.println(err_code);
+    }
+}
+
+/**
+ * @todo does not work; nested interrupt only fires correctly once and then - depending on the call to
+ * resetB either does not fire at all or fires immediately
+ */
+void example_fire_one_channel_with_small_delay_nested_into_larger_delay_of_the_other_channel() {
+    int8_t err_code = 0;
+
+    timer1.reset(); // clean before
+
+    Serial.print("sta A ");
+    Serial.println(millis());
+
+    err_code = timer1.scheduleARecurrent(4000, 5, []() {
+        // WORK: do something
+        Serial.print("end A ");
+        Serial.println(millis());
+
+        timer1.resetB();
+
+        const int8_t err_code = timer1.scheduleBOnce(1100, []() {
+            // WORK: do something
+            Serial.print("  end B ");
+            Serial.println(millis());
+        });
+        if (err_code != Timer1Helper::ER_OK) {
+            Serial.print("  ERR: B ");
+            Serial.println(err_code);
+        }
+    });
+    if (err_code != Timer1Helper::ER_OK) {
+        Serial.print("ERR: A ");
+        Serial.println(err_code);
+    }
+}
+
+void _tandem_schedule_a() {
+    int8_t err_code = 0;
+
+    err_code = timer1.scheduleAOnce(1000, []() {
+        // WORK: do something
+        Serial.print("end A ");
+        Serial.println(millis());
+
+        _tandem_schedule_b();
+    });
+    if (err_code != Timer1Helper::ER_OK) {
+        Serial.print("ERR: A ");
+        Serial.println(err_code);
+    }
+
+}
+
+void _tandem_schedule_b() {
+    int8_t err_code = 0;
+
+    timer1.resetB();
+
+    err_code = timer1.scheduleBOnce(500, []() {
+        // WORK: do something
+        Serial.print("end B ");
+        Serial.println(millis());
+
+        _tandem_schedule_a();
+    });
+    if (err_code != Timer1Helper::ER_OK) {
+        Serial.print("ERR: B ");
+        Serial.println(err_code);
+    }
+}
+
+void example_tandem() {
+    timer1.reset(); // clean before
+
+    Serial.print("sta ");
+    Serial.println(millis());
+
+    _tandem_schedule_a();
+}
+
+int _example_very_large_delay_cnt;
+void example_very_large_delay_and_stop_from_within_handler_routine() {
+    int8_t err_code = 0;
+
+    timer1.reset(); // clean before
+
+    Serial.print("sta ");
+    Serial.println(millis());
+
+    _example_very_large_delay_cnt = 0;
+    err_code = timer1.scheduleAIndefinitely(143, []() {
+        // // WORK: do something
+        ++_example_very_large_delay_cnt;
+        if (_example_very_large_delay_cnt >= 100) {
+            timer1.resetA();
+            Serial.print("end at ");
+            Serial.println(millis());
+        }
+    });
+    if (err_code != Timer1Helper::ER_OK) {
+        Serial.print("ERR: A ");
+        Serial.println(err_code);
+    }
+}
+
+void example_very_large_recurring_delay_without_stop() {
+    int8_t err_code = 0;
+
+    timer1.reset(); // clean before
+
+    Serial.print("sta ");
+    Serial.println(millis());
+
+    _example_very_large_delay_cnt = 0;
+    err_code = timer1.scheduleAIndefinitely(143, []() {
+        // // WORK: do something
+        ++_example_very_large_delay_cnt;
+        if (_example_very_large_delay_cnt == 50) {
+            _example_very_large_delay_cnt = 0;
+            Serial.print("end at ");
+            Serial.println(millis());
+        }
+    });
+    if (err_code != Timer1Helper::ER_OK) {
+        Serial.print("ERR: A ");
+        Serial.println(err_code);
+    }
+}
+
+
+void setup() {
+    Serial.begin(9600);
+    Serial.println("init");
+
+    timer1.reset();
+
+    example_fire_once_channels_a_b();
+    //example_where_prescaler_calculates_incompatible_value_between_the_two_channels_and_will_not_be_set();
+    //example_where_delay_is_beyond_the_upper_max_for_timer1();
+    //example_fire_recurrent_only_channel_A();
+    //example_fire_recurrent_only_channel_B();
+    //example_fire_recurrent_channels_A_B();
+    //example_fire_recurrent_channels_A_B_indenfinitely();
+    //example_fire_one_channel_with_small_delay_nested_into_larger_delay_of_the_other_channel();
+    //example_tandem();
+    //example_very_large_delay_and_stop_from_within_handler_routine();
+    //example_very_large_recurring_delay_without_stop();
+}
+
+void loop() {
+    // do normal tasks, timer will fire callbacks asynchronously
+}
